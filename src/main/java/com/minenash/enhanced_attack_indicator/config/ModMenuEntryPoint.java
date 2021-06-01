52cf06e1409238203a37c3b1caf488618c48a3b5
minenash <minenash@protonmail.com>
2021-06-01 01:10:12 -0400

    Re-added modmenu

--- src/main/java/com/minenash/enhanced_attack_indicator/config/ModMenuEntryPoint.java
+++ src/main/java/com/minenash/enhanced_attack_indicator/config/ModMenuEntryPoint.java
@@ -1,18 +1,13 @@
-//package com.minenash.enhanced_attack_indicator.config;
-//
-//import io.github.prospector.modmenu.api.ConfigScreenFactory;
-//import io.github.prospector.modmenu.api.ModMenuApi;
-//
-//public class ModMenuEntryPoint implements ModMenuApi {
-//
-//    @Override
-//    public String getModId() {
-//        return "seamless_loading_screen";
-//    }
-//
-//    @Override
-//    public ConfigScreenFactory<?> getModConfigScreenFactory() {
-//        return Config::getModConfigScreen;
-//    }
-//
-//}
+package com.minenash.enhanced_attack_indicator.config;
+
+import com.terraformersmc.modmenu.api.ConfigScreenFactory;
+import com.terraformersmc.modmenu.api.ModMenuApi;
+
+public class ModMenuEntryPoint implements ModMenuApi {
+
+    @Override
+    public ConfigScreenFactory<?> getModConfigScreenFactory() {
+        return Config::getModConfigScreen;
+    }
+
+}
